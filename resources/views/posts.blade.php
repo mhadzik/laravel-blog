90d6a6ee6f8f571597d0bf7e4ee61ec9f4cd030d
Mikolaj Hadzik <hadzik.m@mpcreation.net>
2021-11-16 22:06:02 +0100

    First commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea id culpa voluptates dicta corrupti et magnam ab quos perspiciatis. Eveniet distinctio error optio ut quibusdam dolore voluptatibus enim impedit ipsam?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea id culpa voluptates dicta corrupti et magnam ab quos perspiciatis. Eveniet distinctio error optio ut quibusdam dolore voluptatibus enim impedit ipsam?</p>
+    </article>
+</body>
+</html>
