7aed78ac2ee9a31491c2023d0710aab515931819
Mikolaj Hadzik <hadzik.m@mpcreation.net>
2021-11-17 15:51:22 +0100

    Add collection in web.php and make dynamic post title, body, slug, date, excerpt

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('posts', [
         'posts' => Post::all()
     ]);
+
+
 });
 
 Route::get('/posts/{post}', function ($slug) {
